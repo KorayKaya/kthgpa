bdcf8a36e428a76ff79b6a44ff74e2566bd55057
KorayKaya <korre.remix@gmail.com>
2020-10-21 16:18:42 +0200

    test

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect
+import re
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', gpa="0")
 
 
 
