644fed0f6d3c2768a21a040361320cb9a5395c67
KorayKaya <korre.remix@gmail.com>
2020-10-21 13:54:33 +0200

    init working app

---
+++ GPAcalc.py
@@ -0,0 +1,50 @@
+#
+# pip install tika
+# Sen ha intyget som "Intyg.pdf" i samma mapp
+#
+from tika import parser
+import re
+
+gradeDictionary = {
+    "A":5,
+    "B":4.5,
+    "C":4,
+    "D":3.5,
+    "E":3,
+    "F":0,
+    "P":0
+}
+
+rawText = parser.from_file('Intyg.pdf')
+
+rawList = rawText['content'].splitlines()
+finalList  = []
+
+gradeRe = "\\b[A-Z]\\b"
+hpRe = "\\d+,\\d"
+
+totalHp = 0
+totalIgnoredHp = 0
+numerator = 0
+
+for rawLine in rawList:
+    if ("(" not in rawLine and "hp" in rawLine):
+        line = rawLine.replace(u'\xa0', u' ')
+        finalList.append(line)
+        key = re.search(gradeRe, line).group(0)
+        grade = gradeDictionary[key]
+        hp = float(re.search(hpRe, line).group(0).replace(",","."))
+        numerator += hp*grade
+        if (key!="P"):
+            totalHp += hp
+        else:
+            totalIgnoredHp += hp
+        print(line)
+
+print("")
+print("############")
+print("GPA:",numerator/totalHp)
+print("Total used HP:",totalHp)
+print("Ignored P hp:",totalIgnoredHp)
+print("Total studied:",totalHp+totalIgnoredHp)
+input("Press any key to close...")
