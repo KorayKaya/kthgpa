db0ece7c081fea89406bd95fa47bab1ee94e46ab
KorayKaya <korre.remix@gmail.com>
2020-10-21 17:15:09 +0200

    final test

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
 from flask import Flask, render_template, request, redirect
 from tika import parser
+from werkzeug.utils import secure_filename
 import re
+import os
+import tempfile
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        uploaded_file = request.files['file']
-        if uploaded_file.filename == '':
-            return render_template('index.html', gpa="0")
-        return render_template('index.html', gpa=str(calcGPA(uploaded_file)))
+        with tempfile.TemporaryDirectory() as tempdirectory:
+            uploaded_file = request.files['file']
+            if "pdf" not in uploaded_file.filename:
+                return render_template('index.html', gpa="0")
+            filename = secure_filename(uploaded_file.filename)
+            uploaded_file.save(os.path.join(tempdirectory, filename))
+            try:
+                gpa, ignoredhp, totalhp = calcGPA(os.path.join(tempdirectory, filename))
+                return render_template('index.html', gpa=str(gpa), ignoredHP=ignoredhp, totalHP=totalhp)
+            except:
+                return render_template('index.html', gpa="0")
     else:
         return render_template('index.html', gpa="0")
 
@@ -25,6 +35,7 @@ def calcGPA(file):
         "P":0
     }
 
+    print(file)
     rawText = parser.from_file(file)
 
     rawList = rawText['content'].splitlines()
@@ -49,8 +60,7 @@ def calcGPA(file):
                 totalHp += hp
             else:
                 totalIgnoredHp += hp
-            print(line)
     gpa = numerator/totalHp
-    return gpa
+    return gpa,totalIgnoredHp,(totalHp+totalIgnoredHp)
 
 if __name__ == '__main__': app.run(debug=True)
